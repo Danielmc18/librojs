13cf984515157cb635661ee59b42885bdb3e4e2a
Daniel <daniel.montemayorcarlin@hotmail.com>
2021-06-13 18:16:52 -0500

    Archivos vistos en el Capitulo 1 del libro

---
+++ program.js
@@ -0,0 +1,9 @@
+document.writeln("Hello, world!");
+
+
+// Esto es para definir nuevos metodos
+ 
+// Function.prototype.method = function(name, func){
+//     this.prototype(name) = func;
+//     return this;
+// };
