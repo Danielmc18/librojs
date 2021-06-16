e06977ef6e09d2d4a239bd6400099e3e41dd4466
Daniel <daniel.montemayorcarlin@hotmail.com>
2021-06-15 19:06:30 -0500

    Contenido del capitulo 2

---
+++ chapter2.js
@@ -0,0 +1,46 @@
+// es una funcion para que tu puedesas detectar los valores NaN 
+isNaN;
+
+// Este es un metodo para convertir un numero a Entero
+Math.floor
+
+//En los strings puedes usar la " " ó ' '
+nombre = 'Daniel'
+nombre2 = "Daniel2"
+
+// El back-slash es el caracter de escape 
+// Esta es la forma de encerrar caracteres
+" \ "
+' \ '
+
+//Esto provoca las barras invertidas
+" \\ "
+" \/ "
+
+//Esto es para retocreso de la tecla
+" \b "
+
+// Provoca un salto de pagina
+" \f "
+
+//Provoca un salto de linea
+" \n "
+
+//Provoca el retroceso al principio de la linea
+" \r "
+
+// Es el tabulador
+" \t "
+
+// Esto nomas permite un numero hexadecimal de 4 digitos
+" \u1236 " 
+
+//Para concatenar es con el + 
+'c'+'a'+'t' === 'cat'
+
+'cat'.toUpperCase() === 'CAT'
+
+//Declaraciones
+var nombre = 'daniel';
+var apellido;
+var nombre,daniel;
