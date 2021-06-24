b900760469fda5b9f9b5918d216c720ec5649c33
Daniel <daniel.montemayorcarlin@hotmail.com>
2021-06-24 14:00:17 -0500

    notas del capitulo 4

---
+++ chapter4.js
@@ -0,0 +1,14 @@
+//FUNCIONES
+// Tienen 4 partes:
+// 1) La palabra reservada function
+// 2) El nombre de la funcion
+// 3) Son los palametros
+// 4) son los que van entre llaves los procedimientos vdd
+
+var add = function (a,b){
+    return a + b;
+};
+
+var sum = add(3,4);
+
+
