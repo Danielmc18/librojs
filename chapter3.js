008e9cbb168cb5b87d1f8cbd22b81eb6e48e8ae5
Daniel <daniel.montemayorcarlin@hotmail.com>
2021-06-24 13:59:34 -0500

    notas capitulo 3

---
+++ chapter3.js
@@ -0,0 +1,12 @@
+// OBJECTS
+
+// Esta propiedad sirve para saber si una propiedad o no
+// si la tiene dara "true" y sino la tiene sera "false"
+// Object.hasOwnProperty
+
+// El for in es el que se usa para recorrer todas las propiedades de un
+// objeto
+
+// EL operador "DELETE" es para borrar una propiedad de un objeto y asu  vez que se 
+// ponga el del prototipo delete objeto.propiedad
+
