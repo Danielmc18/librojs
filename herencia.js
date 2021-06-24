d5d1a6166ab54c7b8d83098f3c6055f82173cac4
Daniel <daniel.montemayorcarlin@hotmail.com>
2021-06-24 18:34:33 -0500

    Ejemplo de herencia usando objetos

---
+++ herencia.js
@@ -0,0 +1,18 @@
+// O B J E C T S  S P E C I F I E R S
+
+// esto es antes de economizarlo
+var myObject = maker(f,l,m,c,s);
+
+
+// de la mejor forma
+
+var myObject = maker({
+
+    first: f,
+    last: l,
+    middle: m,
+    state: s,
+    city: c
+
+});
+
